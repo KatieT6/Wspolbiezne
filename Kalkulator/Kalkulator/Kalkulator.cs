8185268ec0d40277ef76625e1f2777f33e267e7c
KatieT6 <91150647+KatieT6@users.noreply.github.com>
2023-03-21 23:31:19 +0100

    kalkulator

---
+++ Kalkulator/Kalkulator/Kalkulator.cs
@@ -0,0 +1,29 @@
+﻿namespace Kalkulator
+{
+    public class Kalkulator
+    {
+        public static int Dodawanie(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Odejmowanie(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Mnozenie(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static int Dzielenie(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new ArgumentException("NIE DZIEL PRZEZ 0!");
+            }
+            return a / b;
+        }
+    }
+}
