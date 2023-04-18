b26f47f010f6f6494cba88a0b0084a3c10acde9d
KatieT6 <91150647+KatieT6@users.noreply.github.com>
2023-04-18 23:23:43 +0200

    2.2.1

---
+++ Projekt/tpw_project/Data/Box.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace DataLayer
+{
+    internal class Box
+    {
+        private readonly double sizeX;
+        private readonly double sizeY;
+
+        public double SizeX { get => sizeX; }
+        public double SizeY { get => sizeY; }
+
+        private List<Ball> balls;
+        public List<Ball> Balls { get => balls; }
+
+        public Box(double sizeX, double sizeY)
+        {
+            this.sizeX = sizeX;
+            this.sizeY = sizeY;
+            balls = new List<Ball>();
+        }
+
+        public void addBall(Ball b)
+        {
+            balls.Add(b);
+        }
+    }
+}
