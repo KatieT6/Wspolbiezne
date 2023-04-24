d210a1f40ae96ada09f8760e521c469211990e2b
vdiakivskyi <240363@edu.p.lodz.pl>
2023-04-24 22:34:07 +0200

    data

---
+++ Data/DataAbstractAPI.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    internal class DataAbstractAPI
+    {
+        public static DataAbstractAPI CreateDataAPI()
+        {
+            return new DataLayer();
+        }
+    }
+
+    internal class DataLayer : DataAbstractAPI
+    {
+        public DataLayer() { }
+    }
+}
