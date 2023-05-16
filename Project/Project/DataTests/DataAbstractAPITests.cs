2fa893a580d4d33ff6d2bf2b6d9c39dcb5617894
vdiakivskyi <240363@edu.p.lodz.pl>
2023-05-16 23:19:16 +0200

    update

--- Project/Project/DataTests/DataAbstractAPITests.cs
+++ Project/Project/DataTests/DataAbstractAPITests.cs
@@ -1,4 +1,5 @@
 ﻿using Data;
+using System.Numerics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 
@@ -8,10 +9,39 @@ namespace DataTests
     public class DataAbstractAPITest
     {
         [TestMethod]
-        public void CreateAPITest()
+        public void BoardTest()
         {
-            DataAbstractAPI api = DataAbstractAPI.CreateDataAPI();
-            Assert.IsNotNull(api);
+            Board board = new();
+            board.GenerateBalls(10,3,4,5);
+            Assert.AreEqual(board.Balls, 10);
+        }
+
+        [TestMethod]
+        public void BallConstruktorTest()
+        {
+            Vector2 v = new(1, 2);
+            int radius = 5;
+            Ball ball = new(v, radius);
+            Assert.AreEqual(ball.Radious, radius);
+            Assert.AreEqual(ball.Position, v);
+        }
+
+        [TestMethod]
+
+        public void PositionChangedTest()
+        {
+            Ball ball = new(1500, 4, 5)
+            {
+                Velocity = new Vector2((float)0.003, (float)0.003),
+                Position = new Vector2(50, 50)
+            };
+            ball.ChangePosition();
+            Assert.AreNotEqual(50, ball.Position.X);
+            Assert.AreNotEqual(50, ball.Position.Y);
+            Assert.AreNotEqual(750, ball.Position.X);
+            Assert.AreNotEqual(450, ball.Position.Y);
+            Assert.AreEqual(new Vector2((float)0.003, (float)0.003), ball.Velocity);
+
         }
     }
 }
