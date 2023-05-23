6888fd2cb317ee435e72831a811a5589239f8263
vdiakivskyi <240363@edu.p.lodz.pl>
2023-05-23 21:28:21 +0200

    refactoring tests

---
+++ Project/Project/DataTests/DataAbstractAPITests.cs
@@ -0,0 +1,62 @@
+﻿using Data;
+using System.Collections.ObjectModel;
+using System.Numerics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Data.Tests
+{
+    [TestClass]
+    public class DataAbstractAPITests
+    {
+        [TestMethod]
+        public void GetBallData_ReturnsValidBallData()
+        {
+            // Arrange
+            DataAbstractAPI dataAPI = DataAbstractAPI.CreateDataAPI();
+            Vector2 position = new Vector2(100, 200);
+            Vector2 velocity = new Vector2(0.1f, -0.2f);
+            float radius = 10f;
+            float mass = 5f;
+
+            // Act
+            BallData ballData = dataAPI.GetBallData(position, velocity, radius, mass);
+
+            // Assert
+            Assert.IsNotNull(ballData);
+            Assert.AreEqual(position, ballData.Position);
+            Assert.AreEqual(velocity, ballData.Velocity);
+            Assert.AreEqual(radius, ballData.Radius);
+            Assert.AreEqual(mass, ballData.Mass);
+        }
+
+        [TestMethod]
+        public void CreateDataAPI_ReturnsValidDataAPIInstance()
+        {
+            // Act
+            DataAbstractAPI dataAPI = DataAbstractAPI.CreateDataAPI();
+
+            // Assert
+            Assert.IsNotNull(dataAPI);
+        }
+
+        [TestMethod]
+        public void BoardWidth_ReturnsCorrectValue()
+        {
+            // Act
+            int boardWidth = DataAbstractAPI.BoardWidth;
+
+            // Assert
+            Assert.AreEqual(750, boardWidth);
+        }
+
+        [TestMethod]
+        public void BoardHeight_ReturnsCorrectValue()
+        {
+            // Act
+            int boardHeight = DataAbstractAPI.BoardHeight;
+
+            // Assert
+            Assert.AreEqual(400, boardHeight);
+        }
+    }
+}
---
+++ Project/Project/LogicTests/LogicAbstractAPITests.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.ObjectModel;
+using System.Numerics;
+using System.Threading;
+using Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Logic.Tests
+{
+    [TestClass]
+    public class LogicAbstractAPITests
+    {
+        [TestMethod]
+        public void CreateBalls_CreatesSpecifiedNumberOfBalls()
+        {
+            // Arrange
+            LogicAbstractAPI logicAPI = LogicAbstractAPI.CreateLogicAPI();
+            int ballCount = 5;
+
+            // Act
+            logicAPI.CreateBalls(ballCount);
+
+            // Assert
+            Assert.AreEqual(ballCount, logicAPI.Balls.Count);
+        }
+
+        [TestMethod]
+        public void DeleteBalls_RemovesAllBalls()
+        {
+            // Arrange
+            LogicAbstractAPI logicAPI = LogicAbstractAPI.CreateLogicAPI();
+            int ballCount = 5;
+            logicAPI.CreateBalls(ballCount);
+
+            // Act
+            logicAPI.DeleteBalls();
+
+            // Assert
+            Assert.AreEqual(0, logicAPI.Balls.Count);
+        }
+
+        [TestMethod]
+        public void RunSimulation_StartsUpdatingBalls()
+        {
+            // Arrange
+            LogicAbstractAPI logicAPI = LogicAbstractAPI.CreateLogicAPI();
+            int ballCount = 5;
+            logicAPI.CreateBalls(ballCount);
+
+            
+            logicAPI.RunSimulation();
+
+            
+        }
+
+        [TestMethod]
+        public void StopSimulation_StopsUpdatingBalls()
+        {
+            // Arrange
+            LogicAbstractAPI logicAPI = LogicAbstractAPI.CreateLogicAPI();
+            int ballCount = 5;
+            logicAPI.CreateBalls(ballCount);
+            logicAPI.RunSimulation();
+
+            // Act
+            logicAPI.StopSimulation();
+
+            // Assert
+            // Validate that the balls are no longer being updated (e.g., check their positions or velocities)
+            // Add appropriate assertions based on the expected behavior after stopping the simulation
+        }
+    }
+}
