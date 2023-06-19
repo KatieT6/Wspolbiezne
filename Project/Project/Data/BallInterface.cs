a4ce77ec87b339dfed2e72aff6e0431a76f2d2ca
vdiakivskyi <240363@edu.p.lodz.pl>
2023-06-19 22:21:56 +0200

    ballInterface

---
+++ Project/Project/Data/BallInterface.cs
@@ -0,0 +1,18 @@
+﻿using System.Numerics;
+
+namespace Data
+{
+    public interface BallInterface : IDisposable
+    {
+        int id { get; }
+        int diameter { get; }
+
+
+        Vector2 position { get; }
+        Vector2 velocity { get; }
+
+        int mass { get; }
+
+        event EventHandler? BallChanged;
+    }
+}
